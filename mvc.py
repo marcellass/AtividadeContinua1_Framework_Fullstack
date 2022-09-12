c68f4aed1d75c70696ea31854b48a3f594f13ee0
marcellass <64783591+marcellass@users.noreply.github.com>
2022-09-11 22:20:11 -0300

    Update mvc.py
    mudando variaveis

--- mvc.py
+++ mvc.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
-app.config['MYSQL_DATABASE_DB'] = 'ac1'
+app.config['MYSQL_DATABASE_DB'] = 'alunos'
 app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
@@ -19,12 +19,12 @@ def main():
 @app.route('/gravar', methods=['POST','GET'])
 def gravar():
   nome = request.form['nome']
-  email = request.form['cpf']
-  senha = request.form['endereco']
+  cpf = request.form['cpf']
+  endereco = request.form['endereco']
   if nome and email and senha:
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute('insert into tbl_user (user_name, user_username, user_password) VALUES (%s, %s, %s)', (nome, email, senha))
+    cursor.execute('insert into tbl_user (user_nome, user_cpf, user_endereco) VALUES (%s, %s, %s)', (nome, cpf, endereco))
     conn.commit()
   return render_template('mvc.html')
 
@@ -33,7 +33,7 @@ def gravar():
 def listar():
   conn = mysql.connect()
   cursor = conn.cursor() #abre uma seção dentro da conexão
-  cursor.execute('select user_name, user_username, user_password from tbl_user')
+  cursor.execute('select user_nome, user_cpf, user_endereco alunos_ac1')
   data = cursor.fetchall() #recuperar registros
   conn.commit()
   return render_template('lista.html', datas=data)
